67c2307a1605b4443a64b3314c38e4e67f3496cd
Ayaan9421 <ayaanshaikh9421@gmail.com>
2024-09-11 13:13:39 +0530

    added database connection

--- src/ChatApp.java
+++ src/ChatApp.java
@@ -1,3 +1,6 @@
 public class ChatApp {
-  
+	public static void main(String args[]){
+		DatabaseConnection db = new DatabaseConnection();
+	}
+	
 }
--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,3 +1,16 @@
+import java.sql.*;
 public class DatabaseConnection {
-  
+	public DatabaseConnection(){
+		try{
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			String url = "jdbc:mysql://localhost:3306/chat_app";
+			Connection con = DriverManager.getConnection(url,"root" , "abc123");
+	
+			if(con!=null) {
+				System.out.println("Connection successfull");
+			}
+		} catch(SQLException e) {
+			System.out.println("issues " + e);
+		} 
+	}
 }
